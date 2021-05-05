8778e4ed96d5f36b9dc2f086d0249287432af046
Kaymin Dixon <82177152+steelic@users.noreply.github.com>
2021-05-04 23:18:13 -0400

    Update todo-functions.js

--- todo-functions.js
+++ todo-functions.js
@@ -14,6 +14,19 @@ function saveTodos() {
     localStorage.setItem('todo', JSON.stringify(toDoList))
 }
 
+// Deleting a to-do.
+const deleteTodo = function (id)
+{
+    const todoIndex = toDoList.findIndex(function (todo)
+    {
+        return todo.id === id
+    })
+    if (todoIndex> -1)
+    {
+        toDoList.splice(todoIndex,1)
+    }
+}
+
 // Rendering To-do List
 const renderToDoList = function (toDoList, filter)
 {
@@ -41,8 +54,50 @@ const renderToDoList = function (toDoList, filter)
 
 // Generating DOM structure for a new todo.
 function filteringTodos(todo) {
-    const toDoListElement = document.createElement('p')
-    toDoListElement.textContent = todo.title
+    const toDoListElement = document.createElement('div')
+    const textElement = document.createElement('span')
+
+    // Creating a checkbox to mark to-dos completed.
+    const checkbox = document.createElement('input')
+    checkbox.setAttribute('type','checkbox')
+    if(todo.completion == true)
+    {
+        checkbox.checked = true
+    }
+    checkbox.addEventListener('change', function (e)
+    {
+        if(e.target.checked == true)
+        {
+            todo.completion = true
+            saveTodos()
+            renderToDoList(toDoList, filter)
+        }
+        else if (e.target.checked == false)
+        {
+            todo.completion = false
+            saveTodos()
+            renderToDoList(toDoList, filter)
+        }
+    })
+    
+    // Creating a button to delete to-dos.
+    const button = document.createElement('button')
+    button.textContent = 'Delete'
+    button.addEventListener('click', function (event)
+    {
+        deleteTodo(todo.id)
+        saveTodos()
+        renderToDoList(toDoList, filter)
+    })
+
+    
+    textElement.textContent = todo.title
+
+    // Setting the display order for the checkbox, to-do title, and button.
+    toDoListElement.appendChild(textElement)
+    toDoListElement.appendChild(checkbox)
+    toDoListElement.appendChild(button)
+
     return toDoListElement
 }
 
@@ -63,33 +118,59 @@ function todosRemaining(filteredToDoList) {
 }
 
 // Hiding completed to-dos.
-function hidingCompletedTodos(filteredToDoList, remainingTodos) {
-    const hideTodos = function () {
+function hidingCompletedTodos(filteredToDoList, remainingTodos) 
+{
+    const hideTodos = function () 
+    {
         document.querySelector('#toDoList').innerHTML = ''
 
-        filteredToDoList.forEach(function (todo) {
-            if (todo.completion == hideFilter.completion) {
-                const hiddenTodos = document.createElement('p')
-                hiddenTodos.textContent = todo.title
-                document.querySelector('#toDoList').appendChild(hiddenTodos)
+        filteredToDoList.forEach(function (todo) 
+        {
+            if (todo.completion == hideFilter.completion)
+            {
+                const hiddenTodoElement = document.createElement('p')
+                hiddenTodoElement.textContent = todo.title
+                document.querySelector('#toDoList').appendChild(hiddenTodoElement)
+
+                const checkbox = document.createElement('input')
+                checkbox.setAttribute('type','checkbox')
+                hiddenTodoElement.appendChild(checkbox)
+                checkbox.addEventListener('change', function (e)
+                {
+                    if(e.target.checked)
+                    {
+                        todo.completion = true
+                        saveTodos()
+                        hiddenTodoElement.textContent = null
+                        document.querySelector('#incompleteTodos').innerHTML =  ''
+                        remainingTodos()
+                    }
+                })
+                
+                const button = document.createElement('button')
+                button.textContent = 'Delete'
+                button.addEventListener('click', function (event)
+                {
+                    deleteTodo(todo.id)
+                    saveTodos()
+                    renderToDoList(toDoList, filter)
+                })
+                
+                hiddenTodoElement.appendChild(button)
             }
         })
         remainingTodos()
     }
-    document.querySelector('#hide-todos').addEventListener('change', function (e) {
-        if (e.target.checked) {
+    document.querySelector('#hide-todos').addEventListener('change', function (e) 
+    {
+        if (e.target.checked) 
+        {
             document.querySelector('#incompleteTodos').innerHTML = ''
             hideTodos()
         }
-
-        else {
-            document.querySelector('#toDoList').innerHTML = ''
-            filteredToDoList.forEach(function (todo) {
-                const toDoListElement = document.createElement('p')
-                toDoListElement.textContent = todo.title
-                document.querySelector('#toDoList').appendChild(toDoListElement)
-            })
+        else
+        {
+           renderToDoList(toDoList, filter)
         }
     })
 }
-
